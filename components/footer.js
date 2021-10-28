4fc0ad7d6e6d09e385e66dc8fe7e32eaf7894a22
Andrew Whitely <andrewwhitely@andrews-mbp.lan>
2021-10-28 10:56:32 -0400

    change dark font

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,10 @@
 export default function Footer() {
   return(
     <footer className="flex items-center justify-center w-full h-24 space-x-10 text-xl">
-        <a className="dark:text-purple-400 hover:text-purple-600 flex items-center justify-center" href='/'>home</a>
-        <a className="dark:text-purple-400 hover:text-purple-600 flex items-center justify-center" href='/about'>about</a>
-        <a className="dark:text-purple-400 hover:text-purple-600 flex items-center justify-center" href='/projects'>projects</a>
-        <a className="dark:text-purple-400 hover:text-purple-600 flex items-center justify-center" href='https://twitter.com/ndrewwhitely' target='_blank' rel='noopener noreferrer'>twitter</a> 
+        <a className="hover:text-purple-600 flex items-center justify-center" href='/'>home</a>
+        <a className="hover:text-purple-600 flex items-center justify-center" href='/about'>about</a>
+        <a className="hover:text-purple-600 flex items-center justify-center" href='/projects'>projects</a>
+        <a className="hover:text-purple-600 flex items-center justify-center" href='https://twitter.com/ndrewwhitely' target='_blank' rel='noopener noreferrer'>twitter</a> 
     </footer>
   );
 };
